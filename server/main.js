94bf0188d79c908f0f4654b9e8a4bdeab89366a7
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2018-09-02 13:42:16 +0300

    Got params!

--- server/main.js
+++ server/main.js
@@ -3,15 +3,18 @@ const ws = new WebSocket.Server({port: 3002})
 
 const connections = []
 
-ws.on('connection', (client) => {
+ws.on('connection', (client, req) => {
   console.log('New client connected.')
   connections.push(client)
   client.on('message', data => {
-    console.log(data)
+    const username = req.url.split('&username=')[1]
+    const mediaTitleString = decodeURIComponent(req.url.split('?mediaTitleString=')[1].split('&')[0])
+    console.log('USERNAME: ' + username)
+    console.log('MEDIA: ' + mediaTitleString)
     connections.push({
       client,
-      username: data.username,
-      mediaTitleString: data.mediaTitleString
+      username,
+      mediaTitleString
     })
     client.send('Welcome! You are watching "' + data.mediaTitleString + '" and are now connected to others wathing the same media.')
   })
